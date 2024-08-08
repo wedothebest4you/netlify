d1b4eece08f04c4f948b2c852f48a0cfc3d4cfa3
wedothebest4you <wedothebest4you@gmail.com>
2024-08-08 15:08:55 +0530

    netlify deploy

--- 78831182/functions/getusers.js
+++ 78831182/functions/getusers.js
@@ -3,7 +3,9 @@ exports.handler = function (event, context, callback) {
     statusCode: 200,
     body: 'Hello',
     headers: {
-      'Access-Control-Allow-Origin': 'http://127.0.0.1:5500',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET',
+      'Access-Control-Max-Age': 86400,
       'Access-Control-Allow-Headers':
         'Origin, x-Requested-With, Content-Type,Accept',
     },
